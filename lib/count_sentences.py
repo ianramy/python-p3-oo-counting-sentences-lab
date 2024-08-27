75fb7b5ab9557099a6d530d445fca19fa68cfdf2
ianramy <imwagore@gmail.com>
2024-08-27 06:03:48 +0300

    origin

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,32 @@
-#!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if not isinstance(new_value, str):
+            print("The value must be a string.")
+        else:
+            self._value = new_value
+
+    def is_sentence(self):
+        return self._value.endswith(".")
+
+    def is_question(self):
+        return self._value.endswith("?")
+
+    def is_exclamation(self):
+        return self._value.endswith("!")
+
+    def count_sentences(self):
+        # Regular expression to match sentence-ending punctuation.
+        sentences = re.split(r'(?<=[.!?]) +', self._value)
+        # Filter out empty strings that may result from split
+        sentences = [s for s in sentences if s.strip()]
+        return len(sentences)
